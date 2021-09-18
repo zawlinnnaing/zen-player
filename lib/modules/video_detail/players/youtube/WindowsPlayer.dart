800f304f41effe5f9a028b39c37a77c84c20868e
Zhiyu <zawlinnnaing0018@gmail.com>
2021-09-18 15:58:32 +0630

    - added youtube iframe player

---
+++ lib/modules/video_detail/players/youtube/WindowsPlayer.dart
@@ -0,0 +1,1 @@
+import 'package:flutter/cupertino.dart';class YouTubeWindowsPlayer extends StatefulWidget {  @override  State<StatefulWidget> createState() {    // TODO: implement createState    throw UnimplementedError();  }}
