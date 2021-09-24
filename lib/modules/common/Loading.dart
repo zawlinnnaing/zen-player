536c9bc385ce6e7016b57745508104a2e841a2f4
Zhiyu <zawlinnnaing0018@gmail.com>
2021-09-24 19:03:33 +0630

    - added video detail page

---
+++ lib/modules/common/Loading.dart
@@ -0,0 +1,1 @@
+import 'package:flutter/cupertino.dart';class Loading extends StatelessWidget {  @override  Widget build(BuildContext context) {    return Container(      child: Center(        child: Text("loading"),      ),    );  }}
