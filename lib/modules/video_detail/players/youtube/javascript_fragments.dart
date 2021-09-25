ac876d021213f78b9f886b0229b25d8518243a80
Zhiyu <zawlinnnaing0018@gmail.com>
2021-09-25 18:24:59 +0630

    - update YouTube IframePlayer full screen on landscape

--- lib/modules/video_detail/players/youtube/javascript_fragments.dart
+++ lib/modules/video_detail/players/youtube/javascript_fragments.dart
@@ -1,1 +1,1 @@
-String hidePauseOverlayJs =    "document.querySelector('.ytp-pause-overlay').style.display='none';";
+String hidePauseOverlayJs =    "document.querySelector('.ytp-pause-overlay').style.display='none';";String fullScreenJs =    "player.playVideo(); var playerIframe=document.getElementById('player'); var requestFullScreen = playerIframe.requestFullScreen || playerIframe.mozRequestFullScreen || playerIframe.webkitRequestFullScreen; if (requestFullScreen) { requestFullScreen.bind(playerIframe)(); }";
